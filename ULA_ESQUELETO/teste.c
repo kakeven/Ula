8041df442ca384f4e9c94223d1e61bb3a062a7e7
otavio8v10 <otavio8v10@gmail.com>
2025-02-15 13:51:09 -0300

    Alteracao multiplicacao

--- ULA_ESQUELETO/teste.c
+++ ULA_ESQUELETO/teste.c
@@ -33,12 +33,11 @@ static void somador8bits(uint8_t A, uint8_t B, uint8_t cin, uint8_t * co, uint8_
 
 
 
-int8_t completo2(int8_t num){
+void complementador(uint8_t *num){
 	uint8_t complemento;
     uint8_t co;
-    somador8bits(~num, 1, 0, &co, &complemento); // ~num + 1
-    return complemento;
-
+    somador8bits(~*num, 1, 0, &co, &complemento); // ~num + 1
+    *num = complemento;
 }
 
 void ULA_ADD8B(int8_t *A, int8_t * B, int8_t * overflow){ 
@@ -54,6 +53,8 @@ void ULA_ADD8B(int8_t *A, int8_t * B, int8_t * overflow){
 	
 	if(C1==C2 && C3!=C1){
 		*overflow=1;
+	} else{
+		*overflow=0;
 	}
 
 
@@ -87,77 +88,79 @@ void ULA_ADD16B(int16_t *A, int16_t * B, int8_t * overflow){
 void ULA_SUB(int8_t *A, int8_t * B, int8_t * overflow){ 
 	
 	
-	*B=completo2(*B);
+	complementador(&*B);
+	int8_t C1 = bit_get(*A, 8);
+	int8_t C2 = bit_get(*B, 8);
 	while (*B != 0) {
         int8_t carry = *A & *B;  // Calcula o vai um
         *A = *A ^ *B;          // Soma sem considerar o carry
         *B = carry << 1;     // Move o carry para a esquerda
     }
-    
+    int8_t C3 = bit_get(*A, 8);
+	
+	if(C1==C2 && C3!=C1){
+		*overflow=1;
+	} else{
+		*overflow=0;
+	}
+
 
 
 }
-
-
-
-
-
-
-
-
-
 
 
 
 // void ULA_MUL(int8_t * A, int8_t *Q, int8_t * M,  int8_t * overflow){
-// 	//Baseado no livro Willian Stallings 10ed, cap�tulo 10, pag. 293	
-//     int16_t product = 0; // Inicializa o produto
-//     int16_t mcand = M;
-//     int8_t contador = 8; // Contador para 8 bits
+//     	//Baseado no livro Willian Stallings 10ed, cap�tulo 10, pag. 293
+//     	int16_t product = 0; // Inicializa o produto
+//         int16_t mcand = *Q; // Armazena o multiplicando como int16_t para evitar estouro de bits
+//         int8_t count = 8; // Contador para 8 bits
+//         int16_t add;
+//         int8_t um;
     
-    
-    
-//     while (contador > 0) {
-//         int8_t um=1;
-//         if (*Q & 1) { // Se o bit menos significativo do multiplicador for 1
-//             int16_t multiplicadoq1we2d=mcand;
-//             ULA_ADD16B(&product,&multiplicadoq1we2d,&overflow); // Soma o multiplicando ao produto
+//         while (count > 0) {
+//             if (*M & 1) { 
+//                 add = mcand;
+//                 ULA_ADD16B(&product,&add,&overflow); // Soma o multiplicando ao produto
+//             }
+            
+//             mcand <<= 1;  // Desloca o multiplicando para a esquerda
+//             *M >>= 1;  // Desloca o multiplicador para a direita (mantendo sinal)
+//             um = 1;
+//             ULA_SUB(&count,&um,&overflow);
 //         }
-
-//         mcand <<= 1; // Desloca o multiplicando para a esquerda
-//         *Q >>= 1;   // Desloca o multiplicador para a direita
         
-//         ULA_SUB(&count,&um,&overflow);
+//         *A = (product >> 8) & 0xFF; // Parte alta do resultado
+//         *Q = product & 0xFF;         // Parte baixa do resultado
+//         *overflow = 0;
+            
 //     }
 
-//     return product;
-// }
-
-
-
-
-
-
-
-void binaryMultiplication( int8_t high, int8_tlow, int8_t multiplicand) {
-    int16_t product = 0; // Inicializa o produto
-    int16_t mcand =low; // Armazena o multiplicando como int16_t para evitar estouro de bits
-    int8_t count = 8; // Contador para 8 bits
-
-    while (count > 0) {
-        if (*multiplicand & 1) { // Se o bit menos significativo do multiplicador for 1
-            product += mcand; // Soma o multiplicando ao produto
+    void ULA_MUL(int8_t * A, int8_t *Q, int8_t * M,  int8_t * overflow){
+    	//Baseado no livro Willian Stallings 10ed, cap�tulo 10, pag. 293
+    	int16_t product = 0; // Inicializa o produto
+        int16_t mcand = *Q; // Armazena o multiplicando como int16_t para evitar estouro de bits
+        int8_t count = 8; // Contador para 8 bits
+        int16_t add;
+        int8_t um;
+    
+        while (count > 0) {
+            if (*M & 1) { 
+                add = mcand;
+                ULA_ADD16B(&product,&add,&overflow); // Soma o multiplicando ao produto
+            }
+            
+            mcand <<= 1;  // Desloca o multiplicando para a esquerda
+            *M >>= 1;  // Desloca o multiplicador para a direita (mantendo sinal)
+            um = 1;
+            ULA_SUB(&count,&um,&overflow);
         }
-
-        mcand <<= 1;  // Desloca o multiplicando para a esquerda
-        *multiplicand >>= 1;  // Desloca o multiplicador para a direita (mantendo sinal)
-        count--;
+        
+        *A = (product >> 8) & 0xFF; // Parte alta do resultado
+        *Q = product & 0xFF;         // Parte baixa do resultado
+        *overflow = 0;
+            
     }
-
-    *high = (product >> 8) & 0xFF; // Parte alta do resultado
-    *low = product & 0xFF;         // Parte baixa do resultado
-
-}
 
 
 
@@ -165,25 +168,24 @@ int main(){
     int8_t overflow=0;
 	int8_t high;
     int8_t num1,num2;
-    int16_t multi;
 	
-	//numero_invertido = completo2(num);
+	//numero_invertido = complementador(num);
     //printf("Numero: %d\n",numero_invertido);
-	num1=100;
-	num2=203;
-	ULA_ADD16B(&num1,&num2,&overflow);
+	num1=10;
+	num2=23;
+	ULA_ADD8B(&num1,&num2,&overflow);
 	printf("Soma: %d  overflow: %d\n",num1,overflow);
 	
 	
 	
 	num1=120;
-	num2=120;
+	num2=10;
 	ULA_SUB(&num1,&num2,&overflow);
 	printf("Subt: %d\n",num1);
 
-	num1=120;
-	num2=120;
-	binaryMultiplication(&high,&num1,&num2,&overflow);
+	num1=12;
+	num2=12;
+	ULA_MUL(&high,&num1,&num2,&overflow);
 	//printf("Multiplicao: %d",multi);
     printf("\tMUL: REG A: %d\tREG Q:%d\tAQ: %d\tOverflow: %d\t",high, num1,(int16_t) (((high & 0x00FF) << 8) | (num1 & 0x00FF)),overflow);
 
@@ -191,5 +193,3 @@ int main(){
 
 
 }
-
-
